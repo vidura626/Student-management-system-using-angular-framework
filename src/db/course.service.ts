6176bda8c49ae7a8f2ac6a013ec3c8d090622ceb
vidura626 <wijesinghemap@gmail.com>
2023-08-06 00:39:57 +0530

    Course service coded as inmemory service.

--- src/db/course.service.ts
+++ src/db/course.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import {FormDataInterface} from "../app/student/student/register/formDataInterface";
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +7,32 @@ import { Injectable } from '@angular/core';
 export class CourseService {
 
   constructor() { }
+
+  deleteStudent(id:string){
+
+    this.coursesAr = this.coursesAr.filter((element) => {
+      return element.id !==id;
+    });
+
+  }
+
+  public coursesAr:FormDataInterface[]=[];
+
+  saveCourse(course: FormDataInterface):boolean {
+    console.log(course);
+    this.coursesAr.push(course);
+    return true;
+  }
+
+  searchCourse(id:string) {
+    let find = this.coursesAr.find((element) => {
+      return element.id===id? element: null;
+    });
+    console.log(find);
+    return find;
+  }
+
+  getAllCourses():FormDataInterface[]{
+    return this.coursesAr;
+  }
 }
